c5a11b68ff3175aa3798d987c0537f31e71726d7
Reid Paape <wrpaape@gmail.com>
2015-06-04 12:36:46 -0500

    added 1st user, message, and chatroom

--- db/migrate/20150604172352_create_users.rb
+++ db/migrate/20150604172352_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
-      t.string :settings
+      t.string :name, null: false
+      t.string :password, null: false
+      t.string :settings, default: "default"
       t.text :message_ids
 
       t.timestamps null: false
--- db/migrate/20150604172512_create_messages.rb
+++ db/migrate/20150604172512_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.integer :user_id
       t.text :body
 
       t.timestamps null: false
