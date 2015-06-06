0693f63b3937dae742b73378b1d6476f8597103b
Reid Paape <wrpaape@gmail.com>
2015-06-06 12:06:41 -0500

    pls work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+# This is used to allow the cross origin POST requests made by confroom kiosk app.
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+  end
+
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 end
